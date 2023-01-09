ffbc746074bbc6bc6ff6152eac0a944e71c68a6b
Chaitu785 <77534070+Chaitu785@users.noreply.github.com>
2023-01-10 05:20:07 +0530

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,33 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://codewithharry.com"
-
-# Step 1: Get the HTML
-r = requests.get(url)
-htmlContent = r.content
-# print(htmlContent)
-
-# Step 2: Parse the HTML
-soup = BeautifulSoup(htmlContent, 'html.parser')
 
 
-# Get the title of the HTML page
-title = soup.title
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
 
-# Get all the paragraphs from the page
-paras = soup.find_all('p')
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
 
-anchors = soup.find_all('a')
-all_links = set()
-# Get all the links on the page:
-for link in anchors:
-    if(link.get('href') != '#'): 
-        linkText = "https://codewithharry.com" +link.get('href')
-        all_links.add(link)
-        # print(linkText)
+# Look for Python jobs
+print("PYTHON JOBS\n==============================\n")
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
 
-navbarSupportedContent = soup.find(id='navbarSupportedContent')
-
-
-elem = soup.select('#loginModal')[0] 
-print(elem)
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
+    print()
